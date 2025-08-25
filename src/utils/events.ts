af3540508631e57e06bb7e8307265840d6ddeb7d
Кирпинев Игорь Валерьевич <IKirpinev@alfabank.ru>
2025-08-25 12:57:44 +0300

    init

--- src/utils/events.ts
+++ src/utils/events.ts
@@ -19,7 +19,7 @@ export const sendDataToGA = async (payload: Payload) => {
     }-${now.getDate()} ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`;
 
     await fetch(
-      "https://script.google.com/macros/s/AKfycbyjHKaaXH6pHem-XBGcMsFOgokl_DgRk0ZzlHFgIUMUjbdR1QO9CDErK9t3oUzM4wU_/exec",
+      "https://script.google.com/macros/s/AKfycbwiKyVb9qsDbn-gDOOwAqk-nd-B3G11Cni1U9Ri3QpuMqbkfHHwhnw7quUCClLq5LuD/exec",
       {
         redirect: "follow",
         method: "POST",
